3e2c8f8af55c40f61a150be20e13f326ba42b4cb
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2025-03-10 18:39:56 -0300

    feat: add TwoSum challenge

---
+++ TwoSum/TwoSumChallenge.cs
@@ -0,0 +1,23 @@
+﻿namespace TwoSum
+{
+	public class TwoSumChallenge
+	{
+		//Complexity:  O(n²)
+		// due to nested loops
+		public int[] Execute(int[] nums, int target)
+		{
+			for (int i = 0; i < nums.Length; i++)
+			{
+				for (int j = 1; j < nums.Length; j++)
+				{
+					var sum = nums[i] + nums[j];
+
+					if (sum == target)
+						return [i, j];
+				}
+			}
+
+			return Array.Empty<int>();
+		}
+	}
+}
