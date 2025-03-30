a444da376d6e3589043136351b09cc0382a1e012
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2025-03-30 14:48:05 -0300

    feat: add FirstOccurr challenge

---
+++ FindIndexFirstOccurr/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+		var haystack = "sadbutsad";
+		var needle = "sad";
+
+		var result = StrStr(haystack, needle);
+		Console.WriteLine(result);
+	}
+
+	// Given two strings needle and haystack,
+	// return the index of the first occurrence
+	// of needle in haystack, or -1 if needle
+	// is not part of haystack.
+	public static int StrStr(string haystack, string needle)
+	{
+		for (int i = 0; i < haystack.Length + 1 - (needle.Length); i++)
+		{
+			for (int j = 0; j < needle.Length; j++)
+			{
+				if (haystack[i + j] != needle[j])
+					break;
+
+				if (j == needle.Length - 1)
+					return i;
+			}
+		}
+
+		return -1;
+	}
+}
